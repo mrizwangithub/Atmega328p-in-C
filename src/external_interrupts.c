84c8bc6260a420088e5112d0d64b4be70e7ff053
mrizwangithub <rizwan_ned_ee@yahoo.com>
2022-12-26 00:09:23 -0600

    Changes to access registers by their mem address

--- src/external_interrupts.c
+++ src/external_interrupts.c
@@ -5,36 +5,32 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-// #define mySREG *((volatile uint8_t*) 0x5F)
-// #define myExtIntReg *((volatile uint8_t*) 0x3D) 
-// #define myExtIntControlReg *((volatile uint8_t*) 0x69)
-
-volatile int toggle = 0;
+#define mySREG *((volatile uint8_t*) 0x5F)
+#define myEIMSK *((volatile uint8_t*) 0x3D) 
+#define myEICRA *((volatile uint8_t*) 0x69)
+#define myDDRB *((volatile uint8_t*) 0x24)
 
 int main(void){
 
-    sei(); // Enable global interrupt
-    
-    //External Interrupt Control Register A
-    EICRA |= 0B00000011; // Rising edge configure. EICRA Mem address = 0x69
+    //sei(); // Enable global interrupt
+    //SREG |= (1 << 7); // To enable global interrupt bit
+    mySREG |= (1 << 7);
+
+    // External Interrupt Control Register A
+    //EICRA |= (1 << 1) | 1; // Rising edge configure. EICRA Mem address = 0x69
+    myEICRA |= (1 << 1) | 1;
 
     // External Interrupt Mask Register. To configrue microcontroller to be ready to sense external interrupt as INT1 or INT0 
-    EIMSK |= 0B00000001; // INT1 . EIMSK Address = 0x3D
+    //EIMSK |= 1; // INT0 . EIMSK Address = 0x3D
+    myEIMSK |= 1;
 
-    DDRB |= 0B11111111;
+    // Port B data direction register
+    //DDRB |= (1 << 5);
+    myDDRB |= (1 << 5);
 
     while (1) {;;}
 }
 
- ISR(INT0_vect){
-
-    if(toggle)
-    {
-        PORTB |= 0B00100000;
-    }
-    else{
-        PORTB &= 0B11011111;
-    }
-
-    toggle = 1 - toggle;
+ISR(INT0_vect){
+    PORTB ^= (1 << 5);
  }
